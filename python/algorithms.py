858a5058bb598ce4f60f9ff4290928538e735e76
guilsa <guilsa001@gmail.com>
2020-09-21 15:11:44 -0700

    add python folder with palindrome, insertion and merge sort

---
+++ python/algorithms.py
@@ -0,0 +1,86 @@
+from string import ascii_letters
+import unittest
+
+def merge_sort(list):
+
+  if len(list) > 1:
+    mid = len(list) // 2
+    L = list[:mid]
+    R = list[mid:]
+
+    merge_sort(L)
+    merge_sort(R)
+
+    i = j = k = 0
+
+    while i < len(L) and j < len(R):
+      if L[i] < R[j]:
+        list[k] = L[i]
+        i += 1
+      else:
+        list[k] = R[j]
+        j += 1
+      k += 1
+
+    while i < len(L):
+      list[k] = L[i]
+      i += 1
+      k += 1
+
+    while j < len(R):
+      list[k] = R[j]
+      j += 1 
+      k += 1
+  
+  return list
+
+def insertion_sort(list):
+  for i in range(len(list)):
+    current_value = list[i]
+    pos = i
+    while list[pos - 1] > current_value and pos > 0:
+      list[pos] = list[pos - 1]
+      pos -= 1
+    list[pos] = current_value
+
+  return list
+
+def is_palindrome(s):
+  '''
+  A man, a plan, a canal: Panama
+
+  1. lowercase everything
+  2. remove anything that isnt a letter, including spaces
+  3. if len is odd, find the middle and remove 
+  4. check n and n - 1, loop until middle
+  5. for every iteration, if n != n - 1 return false
+  '''
+  
+  i = 0
+  j = len(s) - 1
+  while i < j:
+    while i < j and not s[i].isalnum():
+      i += 1
+    while i < j and not s[j].isalnum():
+      j -= 1
+    if s[i].lower() != s[j].lower():
+      return False
+    i, j = i + 1, j - 1  
+  return True
+
+
+class Test(unittest.TestCase):
+
+  def test_merge_sort(self):
+    self.assertEqual(merge_sort([8,4,3,5]), [3,4,5,8])
+
+  def test_insertion_sort(self):
+    self.assertEqual(insertion_sort([8, 0, 6, 3]), [0, 3, 6, 8])
+
+  def test_is_palindrome(self):
+    self.assertTrue(is_palindrome('A man, a plan, a canal: Panama'))
+ 
+
+
+if __name__ == '__main__':
+  unittest.main()
