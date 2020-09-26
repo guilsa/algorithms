81f7f10e1b1ad47fd8d0968fda7c26077637ff87
guilsa <guilsa001@gmail.com>
2020-09-25 21:18:55 -0700

    Add find_avg_ofsubarrays. Sliding window problem #1

--- python/algorithms.py
+++ python/algorithms.py
@@ -1,6 +1,17 @@
 from string import ascii_letters
 import unittest
 
+def find_averages_of_subarrays(k, array):
+  result = []
+  window_start, window_sum = 0, 0.0
+  for window_end in range(len(array)):
+    window_sum += array[window_end]
+    if window_end >= k - 1:
+      result.append(window_sum / k)
+      window_sum -= array[window_start]
+      window_start += 1
+  return result
+
 def merge_sort(list):
 
   if len(list) > 1:
@@ -70,6 +81,10 @@ def is_palindrome(s):
 
 
 class Test(unittest.TestCase):
+
+  def test_find_averages_of_subarrays(self):
+    result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+    self.assertEqual(result, [2.2, 2.8, 2.4, 3.6, 2.8])
 
   def test_merge_sort(self):
     self.assertEqual(merge_sort([8,4,3,5]), [3,4,5,8])
